70b0c75b2317ff327743249752c203b4aff9f7b4
Chris Stebe <c.stebe@herzogkommunikation.de>
2015-02-08 22:16:31 +0100

    refactor assets and global js functions, remove dep to jquery.cookie, use localStorage instead

--- BootstrapAsset.php
+++ BootstrapAsset.php
@@ -23,12 +23,64 @@ class BootstrapAsset extends AssetBundle
     public $css = [
     ];
     public $js = [
-        'jquery.cookie/jquery.cookie.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
     ];
     public $jsOptions = [
-        'position' => View::POS_END,
+        'position' => View::POS_HEAD,
     ];
+
+    public function init()
+    {
+
+        parent::init();
+
+        // Register global functions
+        $js = <<<JS
+
+/**
+  * localStorage test, if not available,
+  * feature rememberActiveState has no function
+ */
+var hasStorage = (function() {
+  try {
+    window.localStorage.setItem('tabTest', 'tabTest');
+    window.localStorage.removeItem('tabTest');
+    return true;
+  } catch (exception) {
+    return false;
+  }
+}());
+
+// get sluged dockument URL
+function getControllerId() {
+    var currentUrl = document.URL;
+    return currentUrl
+            .toLowerCase()
+            .replace(/ /g,'-')
+            .replace(/[^\w-]+/g,'');
+}
+// set localstorage item for an element
+function setStorage(elem) {
+    var activeTab     = jQuery(elem).attr("href");
+    if (hasStorage) {
+        window.localStorage.setItem("_bs_activeTab_" + getControllerId(), activeTab);
+    }
+}
+// get this localstorage item
+function initialSelect() {
+    if (hasStorage) {
+        var activeTab = window.localStorage.getItem("_bs_activeTab_" + getControllerId());
+        if (activeTab !== "") {
+            jQuery("[href=" + activeTab + "]").tab("show");
+        }
+    }
+}
+JS;
+        \Yii::$app->controller->getView()->registerJs(
+            $js,
+            View::POS_HEAD,
+            'TabsGlobalFunctions'
+        );
+    }
 }
--- Tabs.php
+++ Tabs.php
@@ -23,35 +23,13 @@ class Tabs extends \yii\bootstrap\Tabs
     public static function rememberActiveState()
     {
         self::registerAssets();
-$js = <<<JS
-            function getControllerId() {
-                currentUrl = document.URL;
-                return currentUrl
-                        .toLowerCase()
-                        .replace(/ /g,'-')
-                        .replace(/[^\w-]+/g,'');
-            }
-
-            function setCookie(elem) {
-            console.log(elem);
-                var activeTab     = jQuery(elem).attr("href");
-                jQuery.cookie.raw = true;
-                jQuery.cookie("_bs_activeTab_" + getControllerId(), activeTab, { path: "/" });
-            }
-
-            function initialSelect() {
-                var activeTab = jQuery.cookie("_bs_activeTab_" + getControllerId());
-                if (activeTab !== "") {
-                    jQuery("[href=" + activeTab + "]").tab("show");
-                }
-            }
-
-            jQuery("#relation-tabs > li > a").on("click", function (event) {
-                setCookie(this);
+        $js = <<<JS
+            jQuery("#relation-tabs > li > a").on("click", function () {
+                setStorage(this);
             });
 
             jQuery(document).on('pjax:end', function() {
-               setCookie($('#relation-tabs .active A'));
+               setStorage($('#relation-tabs .active A'));
             });
 
             jQuery(window).on("load", function () {
@@ -62,63 +40,24 @@ JS;
         if (\Yii::$app->request->isAjax) {
             echo "<script type='text/javascript'>{$js}</script>";
         } else {
-
-        // Register cookie script
-        \Yii::$app->controller->getView()->registerJs(
-            $js,
-            View::POS_READY,
-            'rememberActiveTab'
-        );
+            // Register cookie script
+            \Yii::$app->controller->getView()->registerJs(
+                $js,
+                View::POS_READY,
+                'rememberActiveState'
+            );
         }
     }
 
     /**
-     * @param $controllerId
-     *
-     * @return \yii\web\Cookie
+     * Clear the localStorage of your browser
      */
-    public static function getParentRelationRoute($controllerId)
+    public static function clearLocalStorage()
     {
-        \Yii::$app->request->enableCookieValidation = false;
-
-        if (isset(\Yii::$app->request->cookies["_bs_route_" . $controllerId])) {
-            return \Yii::$app->request->cookies["_bs_route_" . $controllerId]->value;
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @param $controllerId
-     *
-     * @return null|string
-     */
-    public static function getParentRelationActiveTab($controllerId)
-    {
-        \Yii::$app->request->enableCookieValidation = false;
-
-        if (isset(\Yii::$app->request->cookies["_bs_activeTab_" . $controllerId])) {
-            return \Yii::$app->request->cookies["_bs_activeTab_" . $controllerId]->value;
-        } else {
-            return null;
-        }
-    }
-
-    public static function clearParentRelationRoute($controllerId)
-    {
-        self::registerAssets();
-
-        // Register delete cookie script
         \Yii::$app->controller->getView()->registerJs(
-            '
-            var controllerId  = "' . $controllerId . '";
-            jQuery.cookie.raw = true;
-            jQuery.removeCookie("_bs_activeTab_" + controllerId, { path: "/" });
-            jQuery.removeCookie("_bs_route_" + controllerId, { path: "/" });
-            ',
+            'window.localStorage.clear();',
             View::POS_READY,
-            'clearParentRelationRoute'
+            'clearLocalStorage'
         );
     }
-
 }
